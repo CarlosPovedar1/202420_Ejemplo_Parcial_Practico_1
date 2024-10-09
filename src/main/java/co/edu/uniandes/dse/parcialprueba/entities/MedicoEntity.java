ab5302e531f911db5a48e28bf339cec8c6339982
Carlos Poveda <ca.povedar1@uniandes.edu.co>
2024-10-09 13:15:17 -0500

    edad

--- src/main/java/co/edu/uniandes/dse/parcialprueba/entities/MedicoEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/entities/MedicoEntity.java
@@ -1,5 +1,6 @@
 package co.edu.uniandes.dse.parcialprueba.entities;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,7 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Entity
 public class MedicoEntity extends BaseEntity{
     
-  
+   LocalDate fechaNacimiento;
     private String nombre;
     private String registro;
 
--- src/main/java/co/edu/uniandes/dse/parcialprueba/services/MedicoService.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/services/MedicoService.java
@@ -1,6 +1,10 @@
 package co.edu.uniandes.dse.parcialprueba.services;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import co.edu.uniandes.dse.parcialprueba.entities.MedicoEntity;
@@ -17,8 +21,13 @@ public class MedicoService {
     @Transactional
     public MedicoEntity crearMedico(MedicoEntity medicoEntity){ 
         log.info("Crear Medico");
+        LocalDate fechaActual = LocalDate.now();
         if(medicoEntity.getRegistro() == null || medicoEntity.getRegistro().substring(0, 1) == "RW"){
             throw new IllegalArgumentException("El registro  del medico no es valido");
+        }
+        int edad = Period.between(medicoEntity.getFechaNacimiento(), fechaActual).getYears();
+        if(edad<25){
+            throw new IllegalArgumentException("El medico no cumple con la edad minima");
         }
         return medicoRepository.save(medicoEntity);
 
